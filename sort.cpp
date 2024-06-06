2cb5545126c6e1dfe0161f1d970742c664a01a30
Abhinav Kumar Jha <45541461+AbhinavKumarJha@users.noreply.github.com>
2024-06-06 21:00:49 +0530

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -11,20 +11,19 @@
 #include <seastar/core/temporary_buffer.hh>
 
 namespace {
-    seastar::logger lg("sort");
-    static constexpr int record_size = 4096;
+seastar::logger lg("sort");
+static constexpr int record_size = 4096;
 }  // namespace.
 
 // Custom heap comparator for finding minimum during per-shard sorting phase.
 // Value stored in heap denotes the index of the corresponding record in file.
-// The stored value is divided by (run_size_ * record_size_) to get the run number or the index in buffer where the record is stored.
+// The stored value is divided by (run_size_ * record_size_) to get the run
+// number or the index in buffer where the record is stored.
 class PerShardHeapComparator {
  public:
   PerShardHeapComparator(std::vector<std::string>* buffer, int run_size,
-               int record_size) : 
-    buffer_(buffer),
-    run_size_(run_size),
-    record_size_(record_size){}
+                         int record_size)
+      : buffer_(buffer), run_size_(run_size), record_size_(record_size) {}
   bool operator()(const int& lhs, const int& rhs) const {
     return (*buffer_)[lhs / (run_size_ * record_size_)] >
            (*buffer_)[rhs / (run_size_ * record_size_)];
@@ -34,23 +33,25 @@ class PerShardHeapComparator {
   int record_size_;
 };
 
-// Custom heap comparator for finding minimum during shard outputs combine phase.
-// Value stored in heap denotes the cpu id. Since each shard delivers a sorted run. 
-// Minimum from each sorted run is calculated and its cursor points to the next element in the sorted run. 
+// Custom heap comparator for finding minimum during shard outputs combine
+// phase. Value stored in heap denotes the cpu id. Since each shard delivers a
+// sorted run. Minimum from each sorted run is calculated and its cursor points
+// to the next element in the sorted run.
 class CombinerHeapComparator {
  public:
-  CombinerHeapComparator(std::vector<std::string>* buffer) :
-    buffer_(buffer) {}
+  CombinerHeapComparator(std::vector<std::string>* buffer) : buffer_(buffer) {}
   bool operator()(const int& lhs, const int& rhs) const {
     return (*buffer_)[lhs] > (*buffer_)[rhs];
   }
   std::vector<std::string>* buffer_;
 };
 
-// Combines the output from all the shards into output file. Combining happens by 
-// taking first element from all shards into memory, finding minimum using heap, and flushing it to disk, then bring next record from the pop'ed file to memory.
-// This process is repeated till heap has any element left.
-seastar::future<> combine_outputs(int record_size, const seastar::sstring& dir_path) {
+// Combines the output from all the shards into output file. Combining happens
+// by taking first element from all shards into memory, finding minimum using
+// heap, and flushing it to disk, then bring next record from the pop'ed file to
+// memory. This process is repeated till heap has any element left.
+seastar::future<> combine_outputs(int record_size,
+                                  const seastar::sstring& dir_path) {
   auto out_file = co_await seastar::open_file_dma(
       "/home/akj/assgn/output.txt",
       seastar::open_flags::create | seastar::open_flags::rw);
@@ -91,12 +92,15 @@ seastar::future<> combine_outputs(int record_size, const seastar::sstring& dir_p
   }
 }
 
-
-// Performs an iteration where records in the input file( which are already sorted in runs of run_size),
-// are merged in batches of buffer_size and an output file is produced which has records sorted in runs of run_size*buffer_size.
-// This process of iterations is expected to be called by caller until all the records are merged into a single run.
-// It internally maintains an indexed_heap representing elements currently in memory. Heaps gets pop'ed and the corresponding element gets written to disk,
-// and the next element of that run replaces the deleted element in buffer. This process keeps on repeating till heap has any element left.
+// Performs an iteration where records in the input file( which are already
+// sorted in runs of run_size), are merged in batches of buffer_size and an
+// output file is produced which has records sorted in runs of
+// run_size*buffer_size. This process of iterations is expected to be called by
+// caller until all the records are merged into a single run. It internally
+// maintains an indexed_heap representing elements currently in memory. Heaps
+// gets pop'ed and the corresponding element gets written to disk, and the next
+// element of that run replaces the deleted element in buffer. This process
+// keeps on repeating till heap has any element left.
 seastar::future<> do_iteration(const std::string& input_path, int run_size,
                                int start_index, int end_index, int buffer_size,
                                int record_size, const char* output_path) {
@@ -108,8 +112,8 @@ seastar::future<> do_iteration(const std::string& input_path, int run_size,
   std::vector<std::string> buffer(buffer_size);
   int i = start_index, pos = 0;
   while (i <= (int)end_index) {
-    std::priority_queue<int, std::vector<int>, PerShardHeapComparator> indexed_heap(
-        PerShardHeapComparator{&buffer, run_size, record_size});
+    std::priority_queue<int, std::vector<int>, PerShardHeapComparator>
+        indexed_heap(PerShardHeapComparator{&buffer, run_size, record_size});
     int start = i;
     for (; i <= (int)end_index &&
            i - start < buffer_size * run_size * record_size;
@@ -141,22 +145,24 @@ seastar::future<> do_iteration(const std::string& input_path, int run_size,
   co_return;
 }
 
-// Main function which handles the shard division logic and shepherds the per-shard iteration as well as combine phases.
+// Main function which handles the shard division logic and shepherds the
+// per-shard iteration as well as combine phases.
 seastar::future<> external_sort(int buffer_size, seastar::sstring& dir_path) {
   co_await seastar::smp::invoke_on_all([=]() -> seastar::future<> {
     auto file_size = co_await seastar::file_size(dir_path + "input.txt");
-    // lg.info("visdvf {} {}", seastar::sstring(dir_path + "input.txt"), dir_path);
-    // lg.info("visdvf {} {} {}", seastar::sstring(dir_path + "input.txt"), dir_path, file_size);
+    // lg.info("visdvf {} {}", seastar::sstring(dir_path + "input.txt"),
+    // dir_path); lg.info("visdvf {} {} {}", seastar::sstring(dir_path +
+    // "input.txt"), dir_path, file_size);
     int run_size = 1, parity = 0, cpu_idx = seastar::this_shard_id(),
         num_cpu = seastar::smp::count;
-    int record_per_shard = file_size / (num_cpu*record_size);
+    int record_per_shard = file_size / (num_cpu * record_size);
     int start = record_per_shard * cpu_idx * record_size,
         end = record_per_shard * (cpu_idx + 1) * record_size - 1;
     if (cpu_idx == num_cpu - 1) end = file_size - 1;
     std::string tmp_a_path =
         dir_path + "tmp/a" + std::to_string(cpu_idx) + ".txt";
     std::string tmp_b_path =
-       dir_path + "tmp/b" + std::to_string(cpu_idx) + ".txt";
+        dir_path + "tmp/b" + std::to_string(cpu_idx) + ".txt";
     std::string input_path = dir_path + "input.txt";
     std::string output_path =
         dir_path + "tmp/output" + std::to_string(cpu_idx) + ".txt";
@@ -199,7 +205,8 @@ seastar::future<> external_sort(int buffer_size, seastar::sstring& dir_path) {
   co_return;
 }
 
-// Testing function which just confirms that all the records present in final output are in lexicographical order.
+// Testing function which just confirms that all the records present in final
+// output are in lexicographical order.
 seastar::future<bool> test_sort(const seastar::sstring& dir_path) {
   auto file = co_await seastar::open_file_dma(dir_path + "output.txt",
                                               seastar::open_flags::ro);
@@ -223,17 +230,17 @@ seastar::future<bool> test_sort(const seastar::sstring& dir_path) {
 }
 
 int main(int argc, char** argv) {
-
   namespace bpo = boost::program_options;
 
   seastar::app_template::config app_cfg;
-    app_cfg.name = "sort";
+  app_cfg.name = "sort";
 
-    seastar::app_template app(std::move(app_cfg));
-    auto opt_add = app.add_options();
-    opt_add
-        ("buffer-size", bpo::value<int>()->required(), "Number of pages used by buffer per shard")
-        ("dir-path", bpo::value<seastar::sstring>()->required(), "Path to the directory of input file");
+  seastar::app_template app(std::move(app_cfg));
+  auto opt_add = app.add_options();
+  opt_add("buffer-size", bpo::value<int>()->required(),
+          "Number of pages used by buffer per shard")(
+      "dir-path", bpo::value<seastar::sstring>()->required(),
+      "Path to the directory of input file");
 
   return app.run(argc, argv, [&]() -> seastar::future<int> {
     auto& configuration = app.configuration();
@@ -242,10 +249,10 @@ int main(int argc, char** argv) {
 
     co_await external_sort(buffer_size, dir_path);
     auto ret = co_await test_sort(dir_path);
-    if(ret){
-        lg.info("Output is sorted.  SUCCESS!");
+    if (ret) {
+      lg.info("Output is sorted.  SUCCESS!");
     } else {
-        lg.info("Output is *not* sorted.  FAILURE!");
+      lg.info("Output is *not* sorted.  FAILURE!");
     }
     co_return 0;
   });
